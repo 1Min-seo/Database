70fa0207b2b82d9332f2a29503694168f618ae2e
1Min-seo <readex11@gmail.com>
2024-02-26 09:22:36 +0900

    add comment

--- src/sku/lesson/db/DatabaseCenter.java
+++ src/sku/lesson/db/DatabaseCenter.java
@@ -24,9 +24,13 @@ public class DatabaseCenter {
 	public void connectDatabase() throws SQLException, ClassNotFoundException {
 		String id="root";
 		String pwd="1q2w3e4r";
+		
+		//driver : 연결시켜주는(인식시켜주는) 의미
 		String driver="com.mysql.cj.jdbc.Driver";
 		String jdbcURL="jdbc:mysql://localhost:3306/gisa";
 		Class.forName(driver);
+		
+		//db에 직접 연결하는 것이 아닌 관리하는 시스템에 연결
 		Connection con= DriverManager.getConnection(jdbcURL,id,pwd);
 		if(con!=null) {
 			System.out.println("connecting database");
